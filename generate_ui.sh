e7e80c9de02b1ece0cfc58101884b2e09f985ffa
evandrocoan <evandrocoan@hotmail.com>
2021-11-24 20:33:21 -0300

    Moved python files to root to allow cloning the repository
    directly on anki dir.

--- generate_ui.sh
+++ generate_ui.sh
@@ -2,5 +2,5 @@
 
 echo "Generating UI Files"
 
-pyuic5 ui_files/config.ui -o crowd_anki/config/config_ui.py
-pyuic5 ui_files/import.ui -o crowd_anki/importer/import_ui.py
+pyuic5 ui_files/config.ui -o config/config_ui.py
+pyuic5 ui_files/import.ui -o importer/import_ui.py
